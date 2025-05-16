3251247d5388cc3f5bff35ebdcee32e20d307c06
Carter Butaud <cabutaud@microsoft.com>
2025-05-15 19:21:50 -0700

    sankey diagram of income

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useStickyState } from "./hooks";
 import { IncomeForm } from "./income/IncomeForm";
-import { Paycheck } from "./schema";
+import { Paycheck, PaycheckCollection } from "./schema";
 
 function App() {
   const [savedPaycheckData, setSavedPaycheckData] = useStickyState<Paycheck[]>(
@@ -9,18 +9,8 @@ function App() {
     []
   );
 
-  const bonusNetPay = savedPaycheckData.reduce((acc, paycheck) => {
-    const netPay = paycheck.bonusNetPay;
-    return acc + netPay;
-  }, 0);
-  const netStockVestings = savedPaycheckData.reduce((acc, paycheck) => {
-    const netStockVestings = paycheck.netStockVestings;
-    return acc + netStockVestings;
-  }, 0);
-  const nonBonusNetPay = savedPaycheckData.reduce((acc, paycheck) => {
-    const nonBonusNetPay = paycheck.nonBonusNetPay;
-    return acc + nonBonusNetPay;
-  }, 0);
+  const paycheckCollection = new PaycheckCollection(savedPaycheckData);
+
   return (
     <div className="App">
       <button onClick={() => setSavedPaycheckData([])}>
@@ -30,30 +20,47 @@ function App() {
         <IncomeForm onSubmit={(data) => setSavedPaycheckData(data)} />
       )}
       {savedPaycheckData.length > 0 && (
-        <div>
-          <h2>Paycheck Data</h2>
-          <h3>
-            Total Bonus Net Pay: $
-            {bonusNetPay.toLocaleString("en-US", {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}
-          </h3>
-          <h3>
-            Total Non-Bonus Net Pay: $
-            {nonBonusNetPay.toLocaleString("en-US", {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}
-          </h3>
-          <h3>
-            Total Net Stock Vestings: $
-            {netStockVestings.toLocaleString("en-US", {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}
-          </h3>
-        </div>
+        <>
+          <div>
+            <h2>Paycheck Summary</h2>
+            <h3>
+              Total Bonus Net Pay: $
+              {paycheckCollection.bonusNetPay.toLocaleString("en-US", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </h3>
+            <h3>
+              Total Non-Bonus Net Pay: $
+              {paycheckCollection.nonBonusNetPay.toLocaleString("en-US", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </h3>
+            <h3>
+              Total Net Stock Vestings: $
+              {paycheckCollection.netStockVestings.toLocaleString("en-US", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </h3>
+            <h3>
+              Effective Tax Rate:{" "}
+              {(
+                paycheckCollection.taxesWithheld /
+                paycheckCollection.taxableEarnings
+              ).toLocaleString("en-US", {
+                style: "percent",
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </h3>
+          </div>
+          <div>
+            <h2>Sankeymatic code</h2>
+            <pre>{paycheckCollection.sankeyData}</pre>
+          </div>
+        </>
       )}
     </div>
   );
--- src/schema.ts
+++ src/schema.ts
@@ -115,3 +115,195 @@ export class Paycheck {
     return this.stockVesting - (this.stockAwardTaxes ?? 0);
   }
 }
+
+export class PaycheckCollection {
+  private paychecks: Paycheck[];
+
+  constructor(paychecks: Paycheck[]) {
+    this.paychecks = paychecks;
+  }
+
+  get grossPay(): number {
+    return this.paychecks.reduce((acc, paycheck) => acc + paycheck.grossPay, 0);
+  }
+
+  get adjustments(): number {
+    return this.paychecks.reduce(
+      (acc, paycheck) => acc + paycheck.adjustments,
+      0
+    );
+  }
+
+  get bonus(): number {
+    return this.paychecks.reduce((acc, paycheck) => {
+      if (paycheck.bonus) {
+        return acc + paycheck.bonus;
+      }
+      return acc;
+    }, 0);
+  }
+
+  get taxableEarnings(): number {
+    return this.paychecks.reduce(
+      (acc, paycheck) => acc + paycheck.taxableEarnings,
+      0
+    );
+  }
+
+  get taxesWithheld(): number {
+    return this.paychecks.reduce(
+      (acc, paycheck) => acc + paycheck.taxesWithheld,
+      0
+    );
+  }
+  get afterTaxDeductions(): number {
+    return this.paychecks.reduce(
+      (acc, paycheck) => acc + paycheck.afterTaxDeductions,
+      0
+    );
+  }
+
+  get netPay(): number {
+    return this.paychecks.reduce((acc, paycheck) => acc + paycheck.netPay, 0);
+  }
+
+  get bonusNetPay(): number {
+    return this.paychecks.reduce(
+      (acc, paycheck) => acc + paycheck.bonusNetPay,
+      0
+    );
+  }
+
+  get nonBonusNetPay(): number {
+    return this.paychecks.reduce(
+      (acc, paycheck) => acc + paycheck.nonBonusNetPay,
+      0
+    );
+  }
+
+  get stockVesting(): number {
+    return this.paychecks.reduce(
+      (acc, paycheck) => acc + paycheck.stockVesting,
+      0
+    );
+  }
+
+  get netStockVestings(): number {
+    return this.paychecks.reduce(
+      (acc, paycheck) => acc + paycheck.netStockVestings,
+      0
+    );
+  }
+
+  get espp(): number {
+    return this.paychecks.reduce((acc, paycheck) => acc + paycheck.espp, 0);
+  }
+
+  get rothIra(): number {
+    return this.paychecks.reduce((acc, paycheck) => acc + paycheck.rothIra, 0);
+  }
+
+  get salary(): number | undefined {
+    return this.paychecks.reduce((acc, paycheck) => {
+      if (paycheck.salary) {
+        return acc + paycheck.salary;
+      }
+      return acc;
+    }, 0);
+  }
+
+  get grossPayIncludingStock(): number {
+    return this.grossPay + this.stockVesting;
+  }
+
+  get paycheckTaxesWithheld(): number {
+    return this.paychecks.reduce((acc, paycheck) => {
+      if (paycheck.paycheckTaxesWithheld) {
+        return acc + paycheck.paycheckTaxesWithheld;
+      }
+      return acc;
+    }, 0);
+  }
+
+  get stockAwardTaxes(): number {
+    return this.paychecks.reduce((acc, paycheck) => {
+      if (paycheck.stockAwardTaxes) {
+        return acc + paycheck.stockAwardTaxes;
+      }
+      return acc;
+    }, 0);
+  }
+
+  get disabilityInsuranceDeduction(): number {
+    return this.paychecks.reduce((acc, paycheck) => {
+      if (paycheck.constants.disabilityInsuranceDeduction) {
+        return acc + paycheck.constants.disabilityInsuranceDeduction;
+      }
+      return acc;
+    }, 0);
+  }
+
+  get legalPlanDeduction(): number {
+    return this.paychecks.reduce((acc, paycheck) => {
+      if (paycheck.constants.legalPlanDeduction) {
+        return acc + paycheck.constants.legalPlanDeduction;
+      }
+      return acc;
+    }, 0);
+  }
+
+  get givingDeduction(): number {
+    return this.paychecks.reduce((acc, paycheck) => {
+      if (paycheck.constants.givingDeduction) {
+        return acc + paycheck.constants.givingDeduction;
+      }
+      return acc;
+    }, 0);
+  }
+
+  get sankeyData(): string {
+    const lines = [];
+    lines.push(`Salary [${this.salary?.toFixed(2)}] Pre-tax`);
+    lines.push(`Stock [${this.stockVesting.toFixed(2)}] Pre-tax`);
+    lines.push(`Bonus [${this.bonus.toFixed(2)}] Pre-tax`);
+
+    lines.push(`Pre-tax [${this.taxesWithheld.toFixed(2)}] Taxes`);
+    lines.push(`Pre-tax [*] Net Income`);
+
+    lines.push(
+      `Taxes [${this.paycheckTaxesWithheld.toFixed(2)}] Paycheck Taxes`
+    );
+    lines.push(`Taxes [${this.stockAwardTaxes.toFixed(2)}] Stock Taxes`);
+
+    lines.push(
+      `Net Income [${(
+        this.disabilityInsuranceDeduction + this.legalPlanDeduction
+      ).toFixed(2)}] Deducted Services`
+    );
+    lines.push(
+      `Net Income [${this.givingDeduction.toFixed(2)}] Deducted Giving`
+    );
+    lines.push(
+      `Net Income [${(this.rothIra + this.espp).toFixed(
+        2
+      )}] Deducted Investments`
+    );
+    lines.push(
+      `Net Income [${this.netStockVestings.toFixed(2)}] Stock Vestings`
+    );
+    lines.push(`Net Income [*] Paycheck`);
+
+    lines.push(`Deducted Investments [${this.rothIra.toFixed(2)}] Roth IRA`);
+    lines.push(`Deducted Investments [${this.espp.toFixed(2)}] ESPP`);
+
+    lines.push(`ESPP [*] Irregular Income`);
+    lines.push(`Stock Vestings [*] Irregular Income`);
+
+    lines.push(`Paycheck [${this.bonusNetPay.toFixed(2)}] Bonus Take-Home`);
+    lines.push(`Bonus Take-Home [*] Irregular Income`);
+
+    lines.push(`Paycheck [*] Salary Take-Home`);
+
+    return lines.join("\n");
+  }
+}
